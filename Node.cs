999ae9b016d2ec002cb8bd055f28aa71824c2625
unknown <abhisheksrivastava9719@gmail.com>
2022-07-09 10:37:58 +0530

    [Abhishek] added the search and do other functions functionality in an unorderd linked list

---
+++ Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace LinkedList
+{
+    public class Node<gtype>
+    {
+        public gtype data;
+        public Node<gtype> next;
+        public Node(gtype data)
+        {
+            this.data = data;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, welcome to data structure problems");
+﻿using Datastructures;
+using LinkedList;
+Console.WriteLine("Hello, welcome to data structure problems");
+Console.WriteLine("Enter a sentence");
+string sen=Console.ReadLine();
+string[] ar = sen.Split(' ');
+linked_list<string> list = new linked_list<string>();
+list.create(ar[0]);
+for (int i = 1; i <ar.Length; i++)
+{
+    list.append(ar[i]);
+}
+
+Console.WriteLine("Enter a word you want to search");
+string word = Console.ReadLine();
+if(list.search(word)==0)                                            //returns 0 if element is not present
+{
+    list.append(word);
+}
+else
+{
+    list.delete(word, list.search(word));
+}
+list.print();
