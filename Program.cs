12f9cd9510688287b2a7380361636322fd51f130
unknown <abhisheksrivastava9719@gmail.com>
2022-07-11 15:56:37 +0530

    [Abhishek] Added prime numbers between 1-1000 checker

--- Program.cs
+++ Program.cs
@@ -1,26 +1,5 @@
-﻿using linked;
-using DataStructure;
+﻿using Datastructures;
 using System.Collections.Generic;
-Console.WriteLine("Hello, Welcome to the Number in a slot problem");
-NumberInSlot<int> numbers = new NumberInSlot<int>();
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine("Enter number "+(i+1));
-    int num = Convert.ToInt16(Console.ReadLine());
-    numbers.Insert(num);
-}
-Console.WriteLine("Enter a number you want to search");
-int search=Convert.ToInt16(Console.ReadLine());
-long hashcode=search.GetHashCode();
-if(numbers.get(hashcode,search))
-{
-    numbers.remove(search,hashcode);
-    Console.WriteLine("Removed " + search);
-}
-else
-{
-    Console.WriteLine("Not found");
-    numbers.Insert(search);
-    Console.WriteLine("Added "+search);
-}
-numbers.DIsplay();
+Console.WriteLine("Hello, Welcome to the prime numbers between 1-1000 problem");
+prime obj = new prime();
+obj.numArray();
---
+++ prime.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructures
+{
+    public class prime
+    {
+        static int[,] array = new int[10,40];                       //making a 2-D array 
+        public static bool check(int number)
+        {
+            for(int i = 2; i < number/2; i++)
+            {
+                if(number%i==0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public void numArray()
+        {
+            int a = 1, b = 100;                                                 //will be giving the range
+            for (int i = 0; i < 10; i++)                                        //since we have to make ten slots in the outer array each of 100
+            {
+                int index = 0;                              //represents index number of the inner array
+                Console.WriteLine("Array elements in the index number "+i+" in the range "+a+"-"+b);
+                for(int j=a;j<=b;j++)
+                {
+                    if(j==1)                                //since is neither prime nor composite
+                    {
+                        continue;
+                    }
+                    if(check(j)==true)                      //checking for prime number
+                    {
+                        array[i, index] = j;
+                        Console.Write(j+" ");
+                        index++;
+                    }
+                    if (j == b)                             //for changing the range of the numbers ; example- 0-100, 101-200 etc
+                    {
+                        a = a + 100;
+                        b = b + 100;
+                        break;
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
